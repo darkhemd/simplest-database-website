dfb5b4cffd5ee95727b6409427157f07e6f4f504
darkhemd <41227999+darkhemd@users.noreply.github.com>
2019-03-16 15:38:34 +0100

    Add files via upload

---
+++ Scripte/adminback.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<title>Adminback</title>   >_
+<meta charset="UTF-8">
+</html>
+
+<?php
+require 'db.php';
+
+$na = $_GET['name'];
+$pr = $_GET['preis'];
+$ma = $_GET['marken'];
+$ar = $_GET['art'];
+$me = $_GET['medien'];
+$arch = $_GET['architektur'];
+$res = $_GET['res'];
+$rel = $_GET['release'];
+$vor = $_GET['vorher'];
+$nach = $_GET['nachher'];
+$farbe = $_GET['farbe'];
+
+$db->query("INSERT INTO konsole (Name, Preis, Marke, Art, Unterstuetzte_Medien, Hardware_Architektur, Aufloesung, 
+Erscheinungsjahr, Vorgaenger, Nachfolger, Farbe) values ('$na', '$pr', '$ma', '$ar', '$me', 
+'$arch', '$res', '$rel', '$vor', '$nach', '$farbe')");
+
+sleep(1);
+header ( 'Location: admincenter.html' );
+
+
+?>
---
+++ Scripte/anmeldeseite.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>Projekt Anmeldung</title>
+</html>
+
+
+<?php
+require 'db.php';
+
+    $user = $_GET['nutzername'];
+    $psw = $_GET['passwort'];
+
+    $erg = $db->query("SELECT * FROM anmeldung WHERE Name LIKE '%".$user."%'");
+    $erg_arr = $erg->fetch_assoc();
+
+if($psw == $erg_arr["Passwort"])
+{
+    echo "Geht";
+    header("Location: http://localhost/projekt/admincenter.html");
+}
+else
+{
+	header("Location: anmeldefenster.php?nachricht=Falsche Eingabe");
+    //Später folgende Zeile Löschen
+    //echo "<body style=\"background-color: #55555A; \">";
+}
+?>
